d9418b1d57d5af230c01f8df66b024aa67712fd1
bizhari00 <bustamiizhari@gmail.com>
2026-05-23 09:32:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ process_phases = [
         {'label': '', 'shape_type': 'rect', 'tank_area': [271, 93, 428, 169]},
         {'label': '', 'shape_type': 'rect', 'tank_area': [779, 88, 925, 165]},
         # Tambahan Bawah (Bentuk Lingkaran/Circle)
-        {'label': '', 'shape_type': 'circle', 'tank_area': [600,505,763,655]} 
+        {'label': '', 'shape_type': 'circle', 'tank_area': [608,505,763,655]} 
     ],
     
     # --- FASE 3: AKUMULASI STOK/STOCKS (ATAS) + LINGKARAN COST (BAWAH) ---
