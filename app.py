a14b450b19b1ac851085c47f3201838b96cdc56f
bizhari00 <bustamiizhari@gmail.com>
2026-05-31 06:37:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ with col_btn:
     st.link_button("🏠 Tri-Sen Technology Open Here", "https://forio.com/app/trisen_syntegra/trisen2", use_container_width=False)
 
 with col_title:
-    st.markdown('<p class="custom-title">Maintenance-Operational Cost Diagram</p>', unsafe_allow_html=True)
+    st.markdown('<p class="custom-title">Lack of control leads to high costs</p>', unsafe_allow_html=True)
 
 st.divider()
 
